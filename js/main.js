da0fec5968a6757540f39eeccf3f1fdb1962d9ef
Rajan Rastogi <rajan@playlyfe.com>
2014-10-21 13:49:15 +0530

    fix redirect uri

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ app = angular.module('simpleTodo',[]);
 app.run(function(){
   client = new Playlyfe.init({
     client_id: 'MzY1ZmY3MzUtNzE3Ny00NDA0LTlkYzYtNzMxZWQxNzQ2YTQ2',
-    redirect_uri: 'http://localhost:8081'
+    redirect_uri: 'http://playlyfe.github.io/jet-set-gamify/'
   })
 });
