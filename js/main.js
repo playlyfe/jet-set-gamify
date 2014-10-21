5b58fc99c94dfe6045391e325f1c6d3ae3203536
Rajan Rastogi <rajan@playlyfe.com>
2014-10-21 13:45:46 +0530

    update working client ID

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ app = angular.module('simpleTodo',[]);
 
 app.run(function(){
   client = new Playlyfe.init({
-    client_id: 'CLIENT_ID',
+    client_id: 'MzY1ZmY3MzUtNzE3Ny00NDA0LTlkYzYtNzMxZWQxNzQ2YTQ2',
     redirect_uri: 'http://localhost:8081'
   })
 });
