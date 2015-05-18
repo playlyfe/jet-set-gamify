97062d751cfdfa13e170fe9a78ad32bdf463c921
Rajan Rastogi <rajan@playlyfe.com>
2015-05-18 14:29:30 +0530

    fix: update app client ID
    Signed-off-by: Rajan Rastogi <rajan@playlyfe.com>

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ app = angular.module('simpleTodo',[]);
 
 app.run(function(){
   client = new Playlyfe.init({
-    client_id: 'MzY1ZmY3MzUtNzE3Ny00NDA0LTlkYzYtNzMxZWQxNzQ2YTQ2',
+    client_id: 'N2IzZDdiNDMtNGZkNC00MTEwLTg3ZWEtMmU1NzZlNTMyYjkz',
     redirect_uri: 'http://playlyfe.github.io/jet-set-gamify/'
   })
 });
